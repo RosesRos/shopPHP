99b0443b376f3c83ccddb6a65e0aab0248d158d0
facesar <rostranespinoza@gmail.com>
2023-03-19 17:56:54 +0300

    update

--- config/loadTemplate.php
+++ config/loadTemplate.php
@@ -8,10 +8,16 @@ interface LoadTemplates {
 class LoadTemplate implements LoadTemplates {
     public $templa;
     protected $smarty;
+    private $status;
     public function loadTemplate(string $templa) {
         global $smarty;
         $this->templa = $templa;
-        $smarty->display(TEMPLATE_PREFIX. "/{$this->templa}".TEMPLATE_POST_FIX);
+        $status = http_response_code();
+        if ($status !== 200) {
+            print_r('Error with the server' . ' ' . http_response_code(404));
+        } else {
+            $smarty->display(TEMPLATE_PREFIX. "/{$this->templa}".TEMPLATE_POST_FIX);
+        }
     }
 }
 
