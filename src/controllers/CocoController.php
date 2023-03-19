d64da08d01ffbc12dbcfb04db7ee1baf1326023f
facesar <rostranespinoza@gmail.com>
2023-03-19 17:31:09 +0300

    update

--- src/controllers/CocoController.php
+++ src/controllers/CocoController.php
@@ -4,9 +4,9 @@ function cocoAction() {
     print_r('CocoController::cocoaction()');
 }
 
-function testAction($smarty, $load) {
-    // global $smarty;
-    // global $load;
+function testAction() {
+    global $smarty;
+    global $load;
     $smarty->assign('foo', '<h1>test action</h1>');
     // loadTemplate('coco');
     $load->loadTemplate('coco');
