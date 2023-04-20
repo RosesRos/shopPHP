d36339bdad09420a1fe3e3837a218c7578f959ac
RosesRos <rostranespinoza@gmail.com>
2023-04-20 10:09:16 +0300

    update

--- index.sql
+++ index.sql
@@ -10,6 +10,5 @@ SELECT * FROM epiz_33573942_shop.categories WHERE parent_id =
 <<<<<<< HEAD
 SELECT * FROM epiz_33573942_shop.categories WHERE id =
 =======
-SELECT * FROM epiz_33573942_shop.categories WHERE id =
 
 >>>>>>> 8fcde4bf7d1584b94c521dc791b3135dc446ceb2
